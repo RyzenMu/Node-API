e4fc54c466823e5182bb119c33ad21a2d6dc19af
RyzenMu <muruga13in2022@gmail.com>
2024-08-26 08:49:31 +0530

    NewsLetter completed

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const request = require('request');
+// const request = require('request');
+const https = require('https');
 const mailchimpKey ='d288a17cbb3d26acf2f28bb8a869d36c-us13';
 const serverPrefix = 'us13';
+const listId =  '423804c099';
 
-app.listen(8000, ()=>{
-    console.log('server running on port 8000');    
-});
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -20,7 +20,7 @@ app.post('/', (req, res)=>{
     const firstName = String(req.body.firstName);
     const lastName = req.body.lastName;
     const email = req.body.email;
-    const password = req.body.password;
+    const password = req.body.password;    
 
     const data = {
         members :[
@@ -37,5 +37,32 @@ app.post('/', (req, res)=>{
 
     const jsonData = JSON.stringify(data);
 
+    const url = 'https://us13.api.mailchimp.com/3.0/lists/423804c099'
+    const options = {
+        method :"POST",
+        auth : "creative:d288a17cbb3d26acf2f28bb8a869d36c-us13"
+    }
+    const request = https.request(url, options, function(response){
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        }else {
+            res.sendFile(__dirname +'/failure.html')
+        }
+        response.on("data", function(data){
+             console.log(JSON.parse(data));
+             
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 })
+
+app.post('/failure', function(req, res){
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log('server running on port 8000');    
+});
 
